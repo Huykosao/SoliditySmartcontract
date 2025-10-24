a3529bc0f109c1af32469ae8d0c86652c6f5e2c9
Huykosao <huykosao@gmail.com>
2025-10-24 14:54:50 +0700

    .

--- contracts/qlds/CandidateManager.sol
+++ contracts/qlds/CandidateManager.sol
@@ -1,43 +1,50 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract CandidateManager{
+contract CandidateManager {
     struct Candidate {
         string name;
         uint voteCount;
     }
+
     mapping(string => Candidate) public candidates;
     uint8 public totalVotes = 0;
     uint public maxValue;
     string public nameTop;
+
     function addCandidate(string memory _name) public {
         require(!isCandidateExit(_name), "Candidate already exists");
-        candidates[_name] = Candidate(_name,0);
+        candidates[_name] = Candidate(_name, 0);
     }
-    function isCandidateExit(string memory _name) public view returns(bool) {
+
+    function isCandidateExit(string memory _name) public view returns (bool) {
         return bytes(candidates[_name].name).length != 0;
     }
-    function getCandidateInfo(string memory _name) public view returns(string memory,uint) {
+
+    function getCandidateInfo(
+        string memory _name
+    ) public view returns (string memory, uint) {
         require(isCandidateExit(_name), "Candidate does not exist");
-        return (candidates[_name].name,candidates[_name].voteCount);
+        return (candidates[_name].name, candidates[_name].voteCount);
     }
+
     function vote(string memory _name) public {
         require(isCandidateExit(_name), "Candidate does not exist");
         candidates[_name].voteCount++;
         totalVotes++;
+
         if (candidates[_name].voteCount > maxValue) {
             maxValue = candidates[_name].voteCount;
             nameTop = _name;
         }
     }
-    function getTotalVotes() public view returns(uint8){
+
+    function getTotalVotes() public view returns (uint8) {
         return totalVotes;
     }
 
-    function getMaxValue() public view returns(string memory,uint){
+    function getMaxValue() public view returns (string memory, uint) {
         require(bytes(nameTop).length != 0, "No votes yet");
-        return (nameTop,maxValue);
+        return (nameTop, maxValue);
     }
-
-    
 }
--- contracts/qlds/ICandidateManager.sol
+++ contracts/qlds/ICandidateManager.sol
@@ -15,6 +15,7 @@ contract UserManager{
         address userAddress;
         bool isVote;
     }
+
     mapping(uint => User) public users;
     uint public userCount = 0;
     ICandidateManager public candidateContract;
@@ -27,23 +28,18 @@ contract UserManager{
         require(users[_id].userAddress == address(0), "User id already exists");
         require(candidateContract.isCandidateExit(_nameCandidate), "Candidate does not exist");
         users[_id] = User(_id, _name, _nameCandidate, msg.sender, true);
-        // voteCandidate(_id,_nameCandidate);
         candidateContract.vote(_nameCandidate);
         userCount++;
     }
+
     function getUserCount() public view returns (uint) {
         return userCount;
     }
+
     function getUserById(uint _id) public view returns (User memory) {
         return users[_id];
     }
-    // function voteCandidate(uint _id,string memory _nameCandidate) public {
-    //     require(users[_id].isVote == false, "User has voted");
-    //     users[_id].isVote = true;
-    //     users[_id].nameCandidate = _nameCandidate;
-    //     candidateContract.vote(_nameCandidate);
-    // }
-    
+
     function getUserVote(uint _id) public view returns(string memory) {
         require(bytes(users[_id].nameCandidate).length != 0, "User has not voted yet");
         return users[_id].nameCandidate;
